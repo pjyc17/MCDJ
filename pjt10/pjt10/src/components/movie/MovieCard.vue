8b1f8f2b9413f214b4fbb1ca520153a990f1e6c3
김동유 <ehddb2252@gmail.com>
2021-11-13 16:13:03 +0900

    movieCard 말줄임 적용

--- pjt10/pjt10/src/components/movie/MovieCard.vue
+++ pjt10/pjt10/src/components/movie/MovieCard.vue
@@ -3,9 +3,10 @@
 <div class="col-12 col-md-6 col-xl-4 col-xxl-3 movie-card">
   <div class="card h-100">
     <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" class="card-img-top" :alt="movie.title">
-    <div class="card-body reduce-content">
-      <h5 class="card-title">{{movie.title}}</h5>
-      <p class="card-text">{{movie.overview}}</p>
+    <div class="card-body">
+      <h5 class="card-title"><strong>{{movie.title}}</strong></h5>
+      <p v-if="movie.overview" class="card-text reduce-content">{{movie.overview}}</p>
+      <p v-else class="card-text reduce-content">내용 없음</p>
     </div>
   </div>
 </div>
@@ -26,9 +27,10 @@ export default {
 <style scoped>
 .reduce-content {
   overflow: hidden;
-  white-space: wrap;
-  text-overflow: ellipsis;
-  height: 190px;
+  /* text-overflow:ellipsis; */
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
 }
 .movie-card {
   margin: 1rem 0;
