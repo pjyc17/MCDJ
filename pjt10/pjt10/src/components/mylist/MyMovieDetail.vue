b26e7e1854c52d20e1dcb3d44d2142a0c5730ad6
김동유 <ehddb2252@gmail.com>
2021-11-14 04:42:37 +0900

    일단 자러 ㅌㅌ

--- pjt10/pjt10/src/components/mylist/MyMovieDetail.vue
+++ pjt10/pjt10/src/components/mylist/MyMovieDetail.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    {{myMovie}}
+    <div class="float-start w-50">
+      <img :src="`https://image.tmdb.org/t/p/w500/${myMovie.poster_path}`" class="w-100" :alt="myMovie.title">
+    </div>
+    <h5 class="py-3"><strong>{{myMovie.title}}</strong></h5>
+    <p v-if="myMovie.overview" class="overview">{{myMovie.overview}}</p>
   </div>
 </template>
 
@@ -15,6 +19,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.box {
+  display: inline-block;
+}
 </style>
