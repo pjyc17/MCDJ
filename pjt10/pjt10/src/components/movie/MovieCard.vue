362e9332ffb0916346024a9642e9f6710f795ffa
김동유 <ehddb2252@gmail.com>
2021-11-13 16:33:54 +0900

    movieCard높이 맞춤

--- pjt10/pjt10/src/components/movie/MovieCard.vue
+++ pjt10/pjt10/src/components/movie/MovieCard.vue
@@ -2,11 +2,13 @@
 
 <div class="col-12 col-md-6 col-xl-4 col-xxl-3 movie-card">
   <div class="card h-100">
-    <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" class="card-img-top" :alt="movie.title">
+    <div class="card-img-box">
+      <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" class="card-img" :alt="movie.title">
+    </div>
     <div class="card-body">
       <h5 class="card-title"><strong>{{movie.title}}</strong></h5>
       <p v-if="movie.overview" class="card-text reduce-content">{{movie.overview}}</p>
-      <p v-else class="card-text reduce-content">내용 없음</p>
+      <p v-else class="card-text reduce-content"><br><br><br></p>
     </div>
   </div>
 </div>
@@ -34,6 +36,18 @@ export default {
 }
 .movie-card {
   margin: 1rem 0;
+}
+.card-img-box {
+  position: relative;
+  height: 0;
+  padding-top: 150%;
+}
+.card-img {
+  position: absolute;
+  left: 0;
+  top: 0;
+  height: 100%;
+  width: 100%;
 }
 
 </style>
