6698b29fb88eb032ad01b0d78894130bee2dc88b
김동유 <ehddb2252@gmail.com>
2021-11-18 00:13:22 +0900

    11.18 README_D 작성

--- pjt-final/backend/accounts/models.py
+++ pjt-final/backend/accounts/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Age(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='age')
+    age = models.PositiveIntegerField()
