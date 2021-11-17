daf70caa4048ef5711bcf3b69d6b146a2f32dd20
박주윤 <pjyc17@gmail.com>
2021-11-18 03:03:03 +0900

    movie model 작성

--- pjt-final/backend/movies/models.py
+++ pjt-final/backend/movies/models.py
@@ -10,3 +10,27 @@ class movie(models.Model):
     vote_count = models.IntegerField()
     vote_average = models.FloatField()
     popularity = models.FloatField()
+    genres = models.ManyToManyField(genre)
+
+    def __str__(self):
+        return f'title : {self.title}'
+
+class actor(models.Model):
+    name = models.varchar(100)
+    movies = models.ManyToManyField(movie, related_name='actors')
+    
+    def __str__(self):
+        return f'name: {self.name}'
+
+class review(models.Model):
+    movie_id = models.ForeignKey(movie, on_delete=models.CASCADE)
+    title = models.varchar(100)
+    content = models.TextField()
+    rank = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.movie_id}, {self.title}'
+    
+class genre(models.Model):
+    name = models.varchar(50)
+
