aad84b8320f136bb198a2dafb5f84c4f2f5070f6
박주윤 <pjyc17@gmail.com>
2021-11-18 23:34:57 +0900

    serializer 안쓴 accounts, community

--- pjt-final/backend/accounts/urls.py
+++ pjt-final/backend/accounts/urls.py
@@ -5,4 +5,8 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('signup/', views.signup),
     path('api-token-auth/', obtain_jwt_token),
+    path('login/', views.login),
+    path('logout/', views.logout, name='logout'),
+    path('<username>/', views.profile, name='profile'),
+    path('<int:user_pk>/follow/', views.follow, name='follow'),
 ]
--- pjt-final/backend/accounts/views.py
+++ pjt-final/backend/accounts/views.py
@@ -1,3 +1,10 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST, require_http_methods
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -8,7 +15,6 @@ from .serializers import UserSerializer
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def signup(request):
-    print(request.header)
     password = request.data.get('password')
     password2 = request.data.get('password2')
     if password != password2:
@@ -20,3 +26,48 @@ def signup(request):
         user.set_password(request.data.get('password'))
         user.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@require_http_methods(['GET', 'POST'])
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('community:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect(request.GET.get('next') or 'community:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    auth_logout(request)
+    return redirect('community:index')
+
+
+@login_required
+def profile(request, username):
+    person = get_object_or_404(get_user_model(), username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+@require_POST
+def follow(request, user_pk):
+    if request.user.is_authenticated:
+        person = get_object_or_404(get_user_model(), pk=user_pk)
+        user = request.user
+        if person != user:
+            if person.followers.filter(pk=user.pk).exists():
+                person.followers.remove(user)
+            else:
+                person.followers.add(user)
+    return redirect('accounts:profile', person.username)
