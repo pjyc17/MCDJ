74a4829be1daa38c227c97057da6b123a19b1728
박주윤 <pjyc17@gmail.com>
2021-11-19 03:12:19 +0900

    view review 추가

--- pjt-final/backend/community/urls.py
+++ pjt-final/backend/community/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'community'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('create/', views.create, name='create'),
-    path('<int:review_pk>/', views.detail, name='detail'),
+    path('', views.review_list_create),
+    path('<int:review_pk>/', views.review_update_delete),
     path('<int:review_pk>/comments/create/', views.create_comment, name='create_comment'),
     path('<int:review_pk>/like/', views.like, name='like'),
 ]
--- pjt-final/backend/community/views.py
+++ pjt-final/backend/community/views.py
@@ -1,64 +1,42 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_GET, require_POST, require_http_methods
+from django.shortcuts import get_object_or_404
+
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import ReviewSerializer, CommentSerializer
 from .models import Review, Comment
-from .forms import ReviewForm, CommentForm
 
 
-@require_GET
-def index(request):
-    reviews = Review.objects.order_by('-pk')
-    context = {
-        'reviews': reviews,
-    }
-    return render(request, 'community/index.html', context)
-
-
-@require_http_methods(['GET', 'POST'])
-def create(request):
-    if request.method == 'POST':
-        form = ReviewForm(request.POST) 
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.user = request.user
-            review.save()
-            return redirect('community:detail', review.pk)
+@api_view(['GET', 'POST'])
+def review_list_create(request):
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializers.data)
     else:
-        form = ReviewForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'community/create.html', context)
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valild(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-@require_GET
-def detail(request, review_pk):
+@api_view(['PUT', 'DELETE'])
+def review_update_delete(request, review_pk):
     review = get_object_or_404(Review, pk=review_pk)
-    comments = review.comment_set.all()
-    comment_form = CommentForm()
-    context = {
-        'review': review,
-        'comment_form': comment_form,
-        'comments': comments,
-    }
-    return render(request, 'community/detail.html', context)
+    if request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+    else:
+        review.delete()
+        return Response({'id': review_pk})
 
 
-@require_POST
-def create_comment(request, review_pk):
-    review = get_object_or_404(Review, pk=review_pk)
-    comment_form = CommentForm(request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.review = review
-        comment.user = request.user
-        comment.save()
-        return redirect('community:detail', review.pk)
-    context = {
-        'comment_form': comment_form,
-        'review': review,
-        'comments': review.comment_set.all(),
-    }
-    return render(request, 'community/detail.html', context)
+
+
+
+
 
 
 
