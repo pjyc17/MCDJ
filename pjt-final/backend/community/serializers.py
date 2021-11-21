aee6aefe05a70418e6b2a65c18536776b4bad982
박주윤 <pjyc17@gmail.com>
2021-11-21 22:55:06 +0900

    merge

--- pjt-final/backend/community/serializers.py
+++ pjt-final/backend/community/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.fields import ReadOnlyField
 from .models import Review, Comment
 from django.contrib.auth import get_user_model
 
@@ -23,12 +22,13 @@ class ReviewSerializer(serializers.ModelSerializer):
             model = get_user_model()
             fields = ('id', 'username')
             read_only_fields = ('username',)
-    user = User('user')
-    likes_cnt = serializers.IntegerField(source='like_users.count')
+    
+    user = User('user', read_only=True)
+    likes_cnt = serializers.IntegerField(source='like_users.count', read_only=True)
     class Meta:
         model = Review
         fields = ('id', 'user', 'likes_cnt', 'title', 'content', 'created', 'updated',)
-        read_only_fields = ('user', 'likes_cnt')
+        # read_only_fields = ('user', 'likes_cnt')
 
 
 class CommentSerializer(serializers.ModelSerializer):
