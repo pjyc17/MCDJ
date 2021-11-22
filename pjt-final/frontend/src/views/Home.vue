d92e60c8baf1b978cbcf878b3d2655ab649896dc
박주윤 <pjyc17@gmail.com>
2021-11-22 17:25:05 +0900

    merge

--- pjt-final/frontend/src/views/Home.vue
+++ pjt-final/frontend/src/views/Home.vue
@@ -15,7 +15,9 @@
       <b-form-select v-model="y_selected" :options="y_options" multiple :select-size="1"></b-form-select>
       <b-form-select v-model="m_selected" :options="m_options" multiple :select-size="1"></b-form-select>
       <b-form-select v-model="d_selected" :options="d_options" multiple :select-size="1"></b-form-select>
-      <button @click="getBirthday">입력</button>
+      <button @click="getBirthday">
+        입력
+      </button>
     </div>
     <div>
       <div class="mt-3"> 
